1ad64608ec134ef17fbae69e507bce1262a19d49
SeifAlsayed02 <Seifalsayed0202@gmail.com>
2024-01-08 01:31:58 +0200

    Tasks

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        return
     for row in matrix:
         for num in row:
             print("{:d}".format(num), end=" ")
