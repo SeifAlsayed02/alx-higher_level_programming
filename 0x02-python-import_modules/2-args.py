9120c5f1ae5a03a50ecc3b81f39f76248bdf7475
SeifAlsayed02 <Seifalsayed0202@gmail.com>
2024-01-10 19:20:37 +0200

    Tasks

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == '__main__':
+    args = sys.argv
+    arg_len = len(args)
+    if arg_len == 0:
+        print("0 arguments.")
+    elif arg_len == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(arg_len))
+    for i in range(1, arg_len+1):
+        print("{}: {}".format(i, sys.argv[i]))
